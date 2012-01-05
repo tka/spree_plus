774fc40ad3a223110c9d3fc2f066681925f7a6df
tka lu <tkatka@gmail.com>
2012-01-05 23:47:12 +0800

    update equalheight.js

--- app/assets/javascripts/store/equalheight.js
+++ app/assets/javascripts/store/equalheight.js
@@ -1,81 +1,86 @@
-
 // these are (ruh-roh) globals. You could wrap in an
 // immediately-Invoked Function Expression (IIFE) if you wanted to...
 var currentTallest = 0,
-	currentRowStart = 0,
-	rowDivs = new Array();
+currentRowStart = 0,
+rowDivs = new Array();
 
 function setConformingHeight(el, newHeight) {
-	// set the height to something new, but remember the original height in case things change
-	el.data("originalHeight", (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight")));
-	el.height(newHeight);
+
+    // set the height to something new, but remember the original height in case things change
+    el.data("originalHeight", (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight")));
+    el.height(newHeight);
 }
 
 function getOriginalHeight(el) {
-	// if the height has changed, send the originalHeight
-	return (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight"));
+    // if the height has changed, send the originalHeight
+    return (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight"));
 }
 
 function columnConform() {
 
-	// find the tallest DIV in the row, and set the heights of all of the DIVs to match it.
-	$('.inventory-thumb > li').each(function() {
-	
-		// "caching"
-		var $el = $(this);
-		
-		var topPosition = $el.position().top;
+    // find the tallest DIV in the row, and set the heights of all of the DIVs to match it.
+    $('.inventory-thumb > li').each(function() {
 
-		if (currentRowStart != topPosition) {
+        // "caching"
+        var $el = $(this);
 
-			// we just came to a new row.  Set all the heights on the completed row
-			for(currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) setConformingHeight(rowDivs[currentDiv], currentTallest);
+        var topPosition = $el.position().top;
 
-			// set the variables for the new row
-			rowDivs.length = 0; // empty the array
-			currentRowStart = topPosition;
-			currentTallest = getOriginalHeight($el);
-			rowDivs.push($el);
+        if (currentRowStart != topPosition) {
 
-		} else {
+            // we just came to a new row.  Set all the heights on the completed row
+            for(currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) setConformingHeight(rowDivs[currentDiv], currentTallest);
 
-			// another div on the current row.  Add it to the list and check if it's taller
-			rowDivs.push($el);
-			currentTallest = (currentTallest < getOriginalHeight($el)) ? (getOriginalHeight($el)) : (currentTallest);
+            // set the variables for the new row
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPosition;
+            currentTallest = getOriginalHeight($el);
+            rowDivs.push($el);
 
-		}
-		// do the last row
-		for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) setConformingHeight(rowDivs[currentDiv], currentTallest);
+        } else {
 
-	});
+            // another div on the current row.  Add it to the list and check if it's taller
+            rowDivs.push($el);
+            currentTallest = (currentTallest < getOriginalHeight($el)) ? (getOriginalHeight($el)) : (currentTallest);
+
+        }
+        // do the last row
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) setConformingHeight(rowDivs[currentDiv], currentTallest);
+
+    });
 
 }
 
 
 $(window).resize(function() {
-	columnConform();
+    columnConform();
 });
 
 // Dom Ready
 // You might also want to wait until window.onload if images are the things that
 // are unequalizing the blocks
 $(function() {
-	columnConform();
+    columnConform();
 });
 
 
 // http://www.cssnewbie.com/equalheights-jquery-plugin/
 
 function equalHeight(group) {
-   tallest = 0;
-   group.each(function() {
-      thisHeight = $(this).height();
-      if(thisHeight > tallest) {
-         tallest = thisHeight;
-      }
-   });
-   group.height(tallest);
+    if($(window).width() < 740){
+        group.height('auto');
+        return ;
+    }
+
+    tallest = 0;
+    group.each(function() {
+        thisHeight = $(this).height();
+        if(thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.height(tallest);
 }
 $(document).ready(function() {
-   equalHeight($(".column"));
+    equalHeight($(".column"));
 });
